a9dc318e9a88eafc2a703ddc934737981e5da90c
Andrew Fernie <andrewjfernie@gmail.com>
2019-04-08 16:35:19 -0400

    Start merging FrSkySPortPassthru code

--- MavSky/__vm/.MavSky.vsarduino.h
+++ MavSky/__vm/.MavSky.vsarduino.h
@@ -1,9 +1,10 @@
 /* 
 	Editor: https://www.visualmicro.com/
-			visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
-			the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
-			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
-			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+			This file is for intellisense purpose only.
+			Visual micro (and the arduino ide) ignore this code during compilation. This code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+			The contents of the _vm sub folder can be deleted prior to publishing a project
+			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
 	Hardware: Teensy 3.2 / 3.1, Platform=teensy3, Package=teensy
 */
@@ -12,11 +13,12 @@
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __HARDWARE_MK20dx256__
+#define __HARDWARE_mk20dx256__
 #define __HARDWARE_MK20DX256__
+#define _VMDEBUG 1
 #define __MK20DX256__
-#define TEENSYDUINO 142
-#define ARDUINO 10805
+#define TEENSYDUINO 137
+#define ARDUINO 10803
 #define F_CPU 96000000
 #define USB_SERIAL
 #define LAYOUT_US_ENGLISH
@@ -29,13 +31,23 @@ typedef void *__builtin_va_list;
 #define __ATTR_PURE__
 #define __ATTR_CONST__
 #define __inline__
-#define __asm__ 
+#define __asm__(x)
 #define __volatile__
 #define NEW_H
-
+#undef _WIN32
 #define __STDC__ 
+#define __GNUC__ 2
+#define __GNUC_MINOR__ 5
 
-#include <arduino.h>
+extern int at_quick_exit(void (*f)(void));
+int at_quick_exit(void (*f)(void)) {
+}
+extern int quick_exit(void (*f)(void));
+int quick_exit(void (*f)(void)) {
+}
+
+
+#include "arduino.h"
 #define abs(x) ((x)>0?(x):-(x))
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
 #define round(x)     ((x)>=0?(long)((x)+0.5):(long)((x)-0.5))
